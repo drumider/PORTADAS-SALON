eaf1b1ea11f9b2db23816d4df756d8263f81bf16
drumider <122182728+drumider@users.noreply.github.com>
2026-08-22 09:24:29 -0600

    corregido

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore, getFirestore } from 'firebase/firestore';
 import firebaseConfig from '../../firebase-applet-config.json';
 
 const app = initializeApp({
@@ -11,6 +11,16 @@ const app = initializeApp({
   appId: firebaseConfig.appId
 });
 
-export const db = firebaseConfig.firestoreDatabaseId && firebaseConfig.firestoreDatabaseId !== '(default)'
-  ? getFirestore(app, firebaseConfig.firestoreDatabaseId)
-  : getFirestore(app);
+const firestoreSettings = {
+  experimentalAutoDetectLongPolling: true,
+  ignoreUndefinedProperties: true
+};
+
+const customDbId = firebaseConfig.firestoreDatabaseId && firebaseConfig.firestoreDatabaseId !== '(default)'
+  ? firebaseConfig.firestoreDatabaseId
+  : undefined;
+
+export const db = customDbId
+  ? initializeFirestore(app, firestoreSettings, customDbId)
+  : initializeFirestore(app, firestoreSettings);
+
